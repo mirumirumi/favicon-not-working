807df867780dbd3c88f0d3d7bb4cd1846ab56738
mirumi <mail@mirumi.tech>
2022-09-24 17:32:18 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      meta: [],
+      link: [
+        { rel: "icon", href: "/favicon.ico" },
+        { rel: "canonical", href: "https://mydomain.me" },
+      ],
+      style: [],
+      script: [],
+    },
+  },
+})
